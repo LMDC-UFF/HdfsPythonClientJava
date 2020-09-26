3e8dc4f7e1bbc95851986c8bd179ed884f97d01c
Thaylon Guedes Santos <thaylongs@gmail.com>
2020-09-26 13:23:45 -0300

    Add try catch finally

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 # This call to setup() does all the work
 setup(
     name="hdfs-lmdc",
-    version="2.0.4",
+    version="2.0.5",
     description="Esta biblioteca tem como objetivo generalizar funções da integração entre HDFS e Python utilizando HDFS3 ou JavaWrapper",
     long_description=readme(),
     long_description_content_type="text/markdown",
